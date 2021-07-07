8f0dd14b997b06fcd55f08db1d4c58a7135995df
Michael <eoloneox@gmail.com>
2021-07-07 21:37:16 +0200

    c'est cool

---
+++ src/app/cards.ts
@@ -0,0 +1,28 @@
+export interface Cards {
+
+  id: number;
+  name : string;
+  date: Date;
+  imageUrl : string;
+  timelineId : number
+  description : string;
+
+}
+export const cards = [
+  {
+    id: 1,
+    name: 'facebook',
+    Date: new Date('2019-12-12'),
+    timelineId : 1,
+    description: 'c est quand que',
+  },
+  {
+    id: 2,
+    name: 'twitter',
+    Date: new Date('2019-12-12'),
+    timelineId : 1,
+    description: 'c est quand que',
+
+  },
+
+];
